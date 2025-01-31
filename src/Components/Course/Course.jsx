7a6ecbd0249db4ba101689ab767364d2ced0ed23
ChakitPra19 <chakirpra19@gmail.com>
2025-01-31 14:47:23 +0700

    Add all course

--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Course.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Course.css';
 
 const Course = (props) => {
   return (
-    <div className='course'>
+    <div className='course-item'> {/* Updated class name */}
       <div className="course-head">
         <Link to={`/course/${props.course_id}`}>
-          <img onClick={window.scrollTo(0,0)} src={props.image} alt="" />
+          <img onClick={window.scrollTo(0, 0)} src={props.image} alt="" />
         </Link>
-        <p>
-          {props.course_name}
-        </p>
       </div>
       <div className="course-detail">
-          <div className="course-detail-price">
-            <p>Price :</p>
-            ${props.price}
-          </div>
-          <div className="course-detail-subject">
-            <p>Subject :</p>
-            {props.subject}
-          </div>
-          <div className="course-detail-length">
-            <p>Course length :</p>
-            {props.course_length}
-          </div>
-          <div className="course-detail-course_type">
-            <p>Course type :</p>
-            {props.course_type}
-          </div>
+        <p className="course-name">
+          {props.course_name}
+        </p>
+        <div className="course-detail-price">
+          <p>Price :</p>
+          ${props.price}
+        </div>
+        <div className="course-detail-subject">
+          <p>Subject :</p>
+          {props.subject}
+        </div>
+        <div className="course-detail-length">
+          <p>Course length :</p>
+          {props.course_length}
+        </div>
+        <div className="course-detail-course_type">
+          <p>Course type :</p>
+          {props.course_type}
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
