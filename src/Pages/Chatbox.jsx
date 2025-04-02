7ff5959da14758ae4eaf49c07fbf216c3d4dfb35
Pornpipat Kingchongcharoensuk <faith.kingchongcharoensuk@gmail.com>
2025-04-03 02:19:53 +0700

    basic message input

--- src/Pages/Chatbox.jsx
+++ src/Pages/Chatbox.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
+import "./CSS/Chatbox.css";
 
-const Chatbox = () => {
+const Chatbox = ({ socket }) => {
+  const [messages,setMessages] = useState(["Hello","Hi"])
+  const msgIn = useRef("");
+
+  const handleType = (val) => {
+    msgIn.current = val;
+  }
+  const handleSend = () => {
+    // for debug
+    // console.log("message sent", msgIn.current);
+    setMessages([...messages, msgIn.current])
+  }
   return (
-    <div></div>
+    <>
+    <div className="message-section w-full px-5">
+      <ul>
+        {messages.map((msg,index) => <li key={index}>{msg}</li>)}
+      </ul>
+    </div>
+    <div className='w-full px-5'>
+      <input type='text' placeholder='type here' onChange={(e) => {handleType(e.target.value)}} />
+      <button onClick={handleSend} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>Send</button>
+    </div>
+    </>
   )
 }
 
