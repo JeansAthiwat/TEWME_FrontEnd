47252ef6c265ba358bd6ebb762fc31da295f8376
Pornpipat Kingchongcharoensuk <faith.kingchongcharoensuk@gmail.com>
2025-04-04 02:53:09 +0700

    basic chat. TODO: add socket?

--- src/Pages/Chatbox.jsx
+++ src/Pages/Chatbox.jsx
@@ -1,29 +1,97 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import "./CSS/Chatbox.css";
 
-const Chatbox = ({ socket }) => {
-  const [messages,setMessages] = useState(["Hello","Hi"])
-  const msgIn = useRef("");
 
-  const handleType = (val) => {
-    msgIn.current = val;
+const Chatbox = ({ socket }) => {
+  const [messages,setMessages] = useState([]);
+  const [conversations, setConversations] = useState([]);
+  const [currentConv, setCurrentConv] = useState(0);
+  const msgBoxRef = useRef(null);
+
+  useEffect(() => {
+    const getConversations = async() => {
+      const response = await fetch("http://localhost:39189/conversation/user", {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      const data = await response.json();
+      setConversations([...data]);
+      // console.log("User conversations", data);
+    }
+    getConversations();
+  },[])
+
+  useEffect(() => {
+    const getMessages = async() => {
+      const response = await fetch(`http://localhost:39189/message/${conversations[currentConv]._id}`, {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      });
+      const data = await response.json();
+      setMessages([...data]);
+      console.log("messages are", data);
+    }
+    if(conversations.length) getMessages();
+  },[conversations, currentConv])
+
+  const handleSwitchChannel = (val) => {
+    setCurrentConv(val);
   }
-  const handleSend = () => {
+
+  const handleSend = async () => {
     // for debug
     // console.log("message sent", msgIn.current);
-    setMessages([...messages, msgIn.current])
+    const response = await fetch("http://localhost:39189/message", {
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('token')}`,
+          "Content-Type": "application/json" 
+        },
+        body: JSON.stringify({
+          conversationId: conversations[currentConv]._id,
+          text: msgBoxRef.current.value
+        })
+    });
+    const data = await response.json();
+    // console.log("Created new message",data);
+    const updatedData = { ...data, sender: { ...data.sender, email: localStorage.getItem('email') } };
+    // Update the state with the modified message
+    setMessages([...messages, updatedData]);
+    msgBoxRef.current.value = "";
   }
   return (
     <>
-    <div className="message-section w-full px-5">
-      <ul>
-        {messages.map((msg,index) => <li key={index}>{msg}</li>)}
-      </ul>
-    </div>
-    <div className='w-full px-5'>
-      <input type='text' placeholder='type here' onChange={(e) => {handleType(e.target.value)}} />
-      <button onClick={handleSend} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>Send</button>
-    </div>
+    {
+      conversations.length &&
+      <>
+      <div className="chat-list w-full h-10 bg-amber-400 px-5">
+        <select onChange={(e) => {handleSwitchChannel(e.target.value)}}>
+        {conversations.map((conv, index) => 
+          conv.participants.map((participant) => 
+            participant.email !== localStorage.getItem('email') && (
+              <option key={index} value={index}>
+                User: {participant.firstname} {participant.lastname}
+              </option>
+            )
+          )
+        )}
+        </select>
+      </div>
+      <div className="message-section w-full px-5">
+        <ul>
+          {messages.map((msg,index) => (msg.sender.email==localStorage.getItem('email') ? <li key={index} className='bg-blue-200'>{msg.text}</li> : <li key={index}>{msg.text}</li>))}
+        </ul>
+      </div>
+      <div className='w-full px-5'>
+        <input ref={msgBoxRef} type='text' placeholder='type here'/>
+        <button onClick={handleSend} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>Send</button>
+      </div>
+      </>
+    }
     </>
   )
 }
