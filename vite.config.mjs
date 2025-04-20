36dfa814b750c1797536b2e7153ca5585b889e81
Pornpipat Kingchongcharoensuk <faith.kingchongcharoensuk@gmail.com>
2025-04-21 00:11:51 +0700

    add dependency

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), removeConsole()],
   server: {
     port: 3000, // Change this if needed
     proxy: {
